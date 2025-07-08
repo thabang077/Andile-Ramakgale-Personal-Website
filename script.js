219431bef97ba20a8b8c1efc12c5e0d44322d835
andilebaselinedigital <andile@baselinedigital.co.za>
2025-07-08 14:00:54 +0200

    updated card section responsive

--- script.js
+++ script.js
@@ -1,96 +1,95 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-        // sticky navbar on scroll script
-        if (this.scrollY > 20) {
-            $('.navbar').addClass("sticky");
-        } else {
-            $('.navbar').removeClass("sticky");
-        }
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
 
-        // scroll-up button show/hide script
-        if (this.scrollY > 500) {
-            $('.scroll-up-btn').addClass("show");
-        } else {
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
 
-   
-    // slide-up script
-    $('.scroll-up-btn').click(function() {
-        $('html').animate({ scrollTop: 0 });
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
 
-    $('.navbar .menu li a').click(function() {
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+  $(".navbar .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function() {
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Developer", "YouTuber", "Designer", ],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: ["Developer", "YouTuber", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["Developer", "YouTuber", "Designer", ],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+  var typed = new Typed(".typing-2", {
+    strings: ["Developer", "YouTuber", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: false
-            },
-            600: {
-                items: 2,
-                nav: false
-            },
-            1000: {
-                items: 3,
-                nav: false
-            }
-        }
-    });
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
 });
-function sendMail(){
-    var parms = {
-    name : document.getElementById("name").value,
-    email : document.getElementById("email").value,
-    subject : document.getElementById("subject").value,
-    message : document.getElementById("message").value,
-};
-    const ServiceID = "service_3nu9b1s";
-    const TemplateID = "template_lca9zyb";
+function sendMail() {
+  var parms = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+  const ServiceID = "service_3nu9b1s";
+  const TemplateID = "template_lca9zyb";
 
-    emailjs
-   // emailjs.send("service_3nu9b1s","template_lca9zyb", parms).then(alert("Email sent")
+  emailjs
+    // emailjs.send("service_3nu9b1s","template_lca9zyb", parms).then(alert("Email sent")
     .send(ServiceID, TemplateID, parms)
     .then((res) => {
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("subject").value = "";
-        document.getElementById("message").value = "";
-        alert("Email sent");
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("subject").value = "";
+      document.getElementById("message").value = "";
+      alert("Email sent");
     })
     .catch((err) => console.log(err));
 }
